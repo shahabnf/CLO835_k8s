f9a6a03ead8677ce2df12c28bf41fcd6d8b25b30
mansimrankaur24 <simransohal38@gmail.com>
2022-08-10 19:38:40 -0400

    updating dynamic s3 image functionality

--- app.py
+++ app.py
@@ -17,19 +17,15 @@ group_name = os.environ.get('GROUP_NAME')
 if os.path.exists('/clo835/config/image_url'):
     s3_url_file = open('/clo835/config/image_url') 
     json_data = json.load(s3_url_file) #convert to json
+    #download the s3 images locally using aws cli command
+    for url in json_data:
+        cmd = "aws s3 cp " + json_data[url] + f" static/img/{url}.jpg"
+        print("Command :", cmd)
+        process = subprocess.Popen(cmd, shell=True, stdout = subprocess.PIPE)
 else: 
     json_data = {}
 
-try:
-    mysql.connector.connect(host=DB_Host, database=DB_Database, user=DB_User, password=DB_Password)
-    image_url = json_data["success_url"] if json_data else "Not Available"
-except:
-    image_url = json_data["failed_url"] if json_data else "Not Available"
-
-#download the s3 image locally using aws cli command
-cmd = "aws s3 cp " + image_url + " static/img/image.jpg"
-process = subprocess.Popen(cmd, shell=True, stdout = subprocess.PIPE)
-print("Background image url from S3: ", image_url)
+print("Background image urls from S3: ", json_data)
 
 @app.route("/")
 def main():
@@ -39,9 +35,11 @@ def main():
         mysql.connector.connect(host=DB_Host, database=DB_Database, user=DB_User, password=DB_Password)
         color = '#39b54b'
         db_connect_result = True
+        image_url = json_data["success_url"] if json_data else "Not Available"
     except Exception as e:
         color = '#ff3f3f'
         err_message = str(e)
+        image_url = json_data["failed_url"] if json_data else "Not Available"
     
     return render_template('hello.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set") + "; " + err_message, db_connect_result=db_connect_result, name=socket.gethostname(), color=color, group_name=group_name, image_url=image_url)
 
